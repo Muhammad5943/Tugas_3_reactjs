5182d93a59ef777e816ee78ff1f5154e7e5e89c3
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-06-01 12:23:37 +0700

    Mengumpulkan tugas3 reactjs

---
+++ build/precache-manifest.c5eb8ce11a7fc5d9bfe39fe8c39d2c8f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7739d88910eb98058a9b507efa8820b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "0488e4de72dba87f5e57",
+    "url": "/static/js/main.daffb618.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
